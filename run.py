37d468ca93f13816f257ab5cd81fc1b1c5881a81
Khaled Sharif <kldsrf@gmail.com>
2024-05-06 18:06:17 -0700

    cleanup v2

--- run.py
+++ run.py
@@ -1,39 +1,8 @@
-# Copyright (c) 2018-2022, NVIDIA Corporation
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice, this
-#    list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-#    this list of conditions and the following disclaimer in the documentation
-#    and/or other materials provided with the distribution.
-#
-# 3. Neither the name of the copyright holder nor the names of its
-#    contributors may be used to endorse or promote products derived from
-#    this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
-import datetime
 import os
 import gym
 import hydra
 import torch
 from omegaconf import DictConfig
-import omniisaacgymenvs
 from omniisaacgymenvs.envs.vec_env_rlgames import VecEnvRLGames
 from omniisaacgymenvs.utils.config_utils.path_utils import (
     retrieve_checkpoint_path,
@@ -95,11 +64,8 @@ class RLGTrainer:
         )
 
 
-@hydra.main(version_base=None, config_name="config", config_path="../cfg")
+@hydra.main(version_base=None, config_name="config", config_path="./cfg")
 def parse_hydra_configs(cfg: DictConfig):
-
-    time_str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-
     headless = cfg.headless
 
     # local rank (GPU id) in a current multi-gpu mode
@@ -129,9 +95,7 @@ def parse_hydra_configs(cfg: DictConfig):
     )
 
     # parse experiment directory
-    module_path = os.path.abspath(
-        os.path.join(os.path.dirname(omniisaacgymenvs.__file__))
-    )
+    module_path = os.path.abspath(os.curdir)
     experiment_dir = os.path.join(module_path, "runs", cfg.train.params.config.name)
 
     # use gym RecordVideo wrapper for viewport recording
@@ -166,40 +130,20 @@ def parse_hydra_configs(cfg: DictConfig):
 
     cfg_dict = omegaconf_to_dict(cfg)
     print_dict(cfg_dict)
-
-    # sets seed. if seed is -1 will pick a random one
+    
     from omni.isaac.core.utils.torch.maths import set_seed
 
     cfg.seed = cfg.seed + global_rank if cfg.seed != -1 else cfg.seed
     cfg.seed = set_seed(cfg.seed, torch_deterministic=cfg.torch_deterministic)
     cfg_dict["seed"] = cfg.seed
 
-    task = initialize_task(cfg_dict, env)
-
-    if cfg.wandb_activate and global_rank == 0:
-        # Make sure to install WandB if you actually use this.
-        import wandb
-
-        run_name = f"{cfg.wandb_name}_{time_str}"
-
-        wandb.init(
-            project=cfg.wandb_project,
-            group=cfg.wandb_group,
-            entity=cfg.wandb_entity,
-            config=cfg_dict,
-            sync_tensorboard=True,
-            name=run_name,
-            resume="allow",
-        )
+    initialize_task(cfg_dict, env)
 
     torch.cuda.set_device(local_rank)
     rlg_trainer = RLGTrainer(cfg, cfg_dict)
     rlg_trainer.launch_rlg_hydra(env)
     rlg_trainer.run(module_path, experiment_dir)
     env.close()
-
-    if cfg.wandb_activate and global_rank == 0:
-        wandb.finish()
 
 
 if __name__ == "__main__":
